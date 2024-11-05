76d95c6b2a604dca9bea16033ee15b873cbf93dd
Suhrob701 <suhrobnurmamatov909@gmail.com>
2024-11-05 08:15:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+var a = 1
+let b = 2
+const Suhrob = 3
+
+let userName = prompt(`Ismingizni yozing`)
+console.log(`Ismingizni yozing: ${userName}`);
+
+let lastName = prompt("Familiyangizni kiriting");
+
+// Запрашиваем у пользователя его возраст
+let age = prompt("Yoshingizni kiriting");
+
+let subject = prompt("Sizning yoqtirgan faningiz");
+
+let sub = prompt("Yashaydigan tumaningizni kiriting");
+
+alert("Ma'lumotlaringizni kiritganingiz uchun tashakkur");
+
+
+// Выводим фамилию и возраст в консоль
+console.log(`Familyangizni kiriting: ${lastName}`);
+console.log(`Yoshingizni kiriting: ${age}`);
+console.log(`Sizning yoqtirgan faningiz: ${subject}`);
+console.log(`Yashaydigan tumaningizni kiriting: ${sub}`);
